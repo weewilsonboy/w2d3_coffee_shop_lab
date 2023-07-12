6bea10e3c333cbf10389e8120c8acd3c2dc39bec
weewilsonboy <wilson.euan@gmail.com>
2023-07-12 11:20:56 +0100

    added increase and decrease till, and a test for increase

--- src/coffee_shop.py
+++ src/coffee_shop.py
@@ -3,3 +3,9 @@ class CoffeeShop:
 	def __init__(self, name, till):
 		self.name=name
 		self.till = till
+
+	def increase_till(self, amount):
+		self.till += amount
+
+	def decrease_till(self, amount):
+		self.till -= amount
--- tests/coffee_shop_test.py
+++ tests/coffee_shop_test.py
@@ -2,13 +2,17 @@ import unittest
 from src.coffee_shop import CoffeeShop
 
 class TestCoffeeShop(unittest.TestCase):
+    def setUp(self):
+        self.coffee_shop = CoffeeShop("The Prancing Pony",100)
     
     def test_coffee_shop_has_name(self):
-        coffee_shop = CoffeeShop("The Prancing Pony",0)
-        self.assertEqual("The Prancing Pony", coffee_shop.name)
+        self.assertEqual("The Prancing Pony", self.coffee_shop.name)
 
-    def test_coffee_shop_has_till(self):
-        coffee_shop = CoffeeShop("The Prancing Pony", 100)
-        expected = 100
-        actual = coffee_shop.till
-        self.assertAlmostEqual(expected, actual)
+    def test_coffee_shop_has_till(self): #make a test, the name implies it will check that there is a till
+        expected = 100 #the number we expect to be as coffee_shop.till
+        actual = self.coffee_shop.till #what coffee_shop.till actually is
+        self.assertAlmostEqual(expected, actual) #check that the actual is the same as expected
+
+    def test_coffee_shop_increase_till(self):
+        self.coffee_shop.increase_till(2.5)
+        self.assertEqual(102.5, self.coffee_shop.till)
