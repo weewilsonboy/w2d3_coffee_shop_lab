304e08ffc1fd1acce4e9ccba1452acbe5b1cca50
weewilsonboy <wilson.euan@gmail.com>
2023-07-12 13:12:39 +0100

    drinks created and tested

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -5,6 +5,10 @@ class TestDrink(unittest.TestCase):
     def setUp(self):
         self.drink = Drink("Vanilla mocha cappuchino flatte", 4)
 
-    def test_coffee_shop_has_name(self):
-        self.assertEqual("Different", self.drink.name)
+    def test_drink_has_name(self):
+        self.assertEqual("Vanilla mocha cappuchino flatte", self.drink.name)
+
+    
+    def test_drink_has_price(self):
+        self.assertEqual(4.0, self.drink.price)
 
